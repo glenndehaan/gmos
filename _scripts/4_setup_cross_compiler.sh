b569e9f6f5760cd8909f6455b415329ae0aaea6e
Glenn de Haan <glenn@dehaan.cloud>
2018-10-16 12:57:26 +0200

    Added next compile step

--- _scripts/4_setup_cross_compiler.sh
+++ _scripts/4_setup_cross_compiler.sh
@@ -30,12 +30,20 @@ cd ../_resources/linux-4.16.3
 #ln -vs libgcc.a `${GMOS_TARGET}-gcc -print-libgcc-file-name | sed 's/libgcc/&_eh/'`
 
 # Glibc
+#cd ..
+#mkdir glibc-build
+#cd glibc-build/
+#echo "libc_cv_forced_unwind=yes" > config.cache
+#echo "libc_cv_c_cleanup=yes" >> config.cache
+#echo "libc_cv_ssp=no" >> config.cache
+#echo "libc_cv_ssp_strong=no" >> config.cache
+#BUILD_CC="gcc" CC="${GMOS_TARGET}-gcc" AR="${GMOS_TARGET}-ar" RANLIB="${GMOS_TARGET}-ranlib" CFLAGS="-O2" ../glibc-2.27/configure --prefix=/usr --host=${GMOS_TARGET} --build=${GMOS_HOST} --disable-profile --enable-add-ons --with-tls --enable-kernel=2.6.32 --with-__thread --with-binutils=${GMOS}/cross-tools/bin --with-headers=${GMOS}/usr/include --cache-file=config.cache
+#make && make install_root=${GMOS}/ install
+
+# GCC (Final)
 cd ..
-mkdir glibc-build
-cd glibc-build/
-echo "libc_cv_forced_unwind=yes" > config.cache
-echo "libc_cv_c_cleanup=yes" >> config.cache
-echo "libc_cv_ssp=no" >> config.cache
-echo "libc_cv_ssp_strong=no" >> config.cache
-BUILD_CC="gcc" CC="${GMOS_TARGET}-gcc" AR="${GMOS_TARGET}-ar" RANLIB="${GMOS_TARGET}-ranlib" CFLAGS="-O2" ../glibc-2.27/configure --prefix=/usr --host=${GMOS_TARGET} --build=${GMOS_HOST} --disable-profile --enable-add-ons --with-tls --enable-kernel=2.6.32 --with-__thread --with-binutils=${GMOS}/cross-tools/bin --with-headers=${GMOS}/usr/include --cache-file=config.cache
-make && make install_root=${GMOS}/ install
+mkdir gcc-build
+cd gcc-build/
+AR=ar LDFLAGS="-Wl,-rpath,${GMOS}/cross-tools/lib" ../gcc-7.3.0/configure --prefix=${GMOS}/cross-tools --build=${GMOS_HOST} --target=${GMOS_TARGET} --host=${GMOS_HOST} --with-sysroot=${GMOS} --disable-nls --enable-shared --enable-languages=c,c++ --enable-c99 --enable-long-long --with-mpfr-include=$(pwd)/../gcc-7.3.0/mpfr/src --with-mpfr-lib=$(pwd)/mpfr/src/.libs --disable-multilib --with-arch=${GMOS_CPU}
+make && make install
+cp -v ${GMOS}/cross-tools/${GMOS_TARGET}/lib64/libgcc_s.so.1 ${GMOS}/lib64
